6ea04f44e91d0da94e5778e5fcce5fbf3c2964e0
ant4u <anthonyimontalto@gmail.com>
2018-12-15 06:57:12 -0500

    letter testing

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ inquirer.prompt([
       message: "Guess a letter"
     }
   ]).then(function(answers) {
-    var newLetter = new Letter(answers.letter, false);
+    var newLetter = new Letter("A", false);
     // printInfo method is run to show that the newProgrammer object was successfully created and filled
+    newLetter.checkChar(answers.letter);
     var char =  newLetter.toString();
     console.log(char);
   });
